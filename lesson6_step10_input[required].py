d2324723bfa750bc18758c2a76d656306831c6d3
unknown <mcpropilin@gmail.com>
2022-07-04 13:42:02 +0300

    1lessons added

---
+++ lesson6_step10_input[required].py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+   
+    input1 = browser.find_element(By.CSS_SELECTOR, "input[required].first")
+    input1.send_keys("Maxim")
+    input2 = browser.find_element(By.CSS_SELECTOR, "input[required].second")
+    input2.send_keys("Shcherbakov")
+    input3 = browser.find_element(By.CSS_SELECTOR, "input[required].third")
+    input3.send_keys("mcpropilin@gmail.com")
+    button = browser.find_element(By.CSS_SELECTOR, '[type="submit"]')
+    button.click()
+# Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
